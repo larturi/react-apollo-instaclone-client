0935934e6388842461feace556d6e86509bc059b
lea.arturi <lea.arturi@gmail.com>
2021-08-20 13:48:26 -0300

    Muestra cantidad de seguidores

--- src/components/User/Profile/Followers/Followers.jsx
+++ src/components/User/Profile/Followers/Followers.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_FOLLOWERS } from '../../../../gql/follow';
+import { size } from 'lodash';
 
 import './Followers.scss';
 
 export default function Followers(props) {
    const { username } = props;
+
+   const { data: dataFollowers, loading: loadingFollowers } = useQuery(
+      GET_FOLLOWERS,
+      {
+         variables: { username },
+      }
+   );
+
+   if (loadingFollowers) return null;
+   const { getFollowers } = dataFollowers;
+
    return (
       <div className='followers'>
          <p>
             <span>50</span> publicaciones
          </p>
          <p className='link'>
-            <span>504</span> seguidores
+            <span>{size(getFollowers)}</span> seguidores
          </p>
          <p className='link'>
-            <span>454</span> seguidos
+            <span>**</span> seguidos
          </p>
       </div>
    );
