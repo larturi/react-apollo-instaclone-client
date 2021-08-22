c846e6b32f804366418e5d236a2082d505e75a38
lea.arturi <lea.arturi@gmail.com>
2021-08-22 18:27:54 -0300

    DropZone

--- src/components/Modals/ModalUpload/ModalUpload.jsx
+++ src/components/Modals/ModalUpload/ModalUpload.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Modal, Icon, Button, Dimmer, Loader } from 'semantic-ui-react';
+import { useDropzone } from 'react-dropzone';
 
 import './ModalUpload.scss';
 
 export default function ModalUpload(props) {
    const { show, setShow } = props;
+
+   const onDrop = useCallback((acceptedFile) => {
+      const file = acceptedFile[0];
+      console.log(file);
+   });
+
+   const { getRootProps, getInputProps } = useDropzone({
+      accept: 'image/jpg, image/png',
+      noKeyboard: true,
+      multiple: false,
+      onDrop,
+   });
 
    const onClose = () => {
       setShow(false);
@@ -17,7 +30,11 @@ export default function ModalUpload(props) {
          onClose={onClose}
          className='modal-upload'
       >
-         <h1>Modal Upload</h1>
+         <div {...getRootProps()} className='dropzone'>
+            <Icon name='cloud upload' />
+            <p>Arrastra la foto que quieras publicar</p>
+            <input {...getInputProps()} />
+         </div>
       </Modal>
    );
 }
