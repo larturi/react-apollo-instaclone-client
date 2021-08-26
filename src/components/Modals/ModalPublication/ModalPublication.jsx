7637e244b49c275394e97f4cce99dc5282d5b10e
lea.arturi <lea.arturi@gmail.com>
2021-08-26 18:10:38 -0300

    Modal Publicacion muestra imagen

---
+++ src/components/Modals/ModalPublication/ModalPublication.jsx
@@ -0,0 +1,31 @@
+import { GraphQLList } from 'graphql';
+import React from 'react';
+import { Modal, Grid } from 'semantic-ui-react';
+
+import './ModalPublication.scss';
+
+export default function ModalPublication(props) {
+   const { show, setShow, publication } = props;
+
+   const onClose = () => {
+      setShow(false);
+   };
+
+   return (
+      <Modal open={show} onClose={onClose} className='modal-publication'>
+         <Grid>
+            <Grid.Column
+               className='modal-publication__left'
+               width={10}
+               style={{ backgroundImage: `url("${publication.file}")` }}
+            />
+
+            <Grid.Column className='modal-publication__right' width={6}>
+               <div>Comentarios</div>
+               <div>Actions</div>
+               <div>Form comment</div>
+            </Grid.Column>
+         </Grid>
+      </Modal>
+   );
+}
---
+++ src/components/Modals/ModalPublication/index.js
@@ -0,0 +1,1 @@
+export { defsult } from './publication.js';
