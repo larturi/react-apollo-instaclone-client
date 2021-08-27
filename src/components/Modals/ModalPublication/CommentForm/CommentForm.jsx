65e1e0814ad3d06d06b4f80632e69a9d17612171
lea.arturi <lea.arturi@gmail.com>
2021-08-27 13:52:22 -0300

    Graba comentario en la bd desde el form

--- src/components/Modals/ModalPublication/CommentForm/CommentForm.jsx
+++ src/components/Modals/ModalPublication/CommentForm/CommentForm.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Form, Button } from 'semantic-ui-react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import { useMutation } from '@apollo/client';
+import { ADD_COMMENT } from '../../../../gql/comment';
 
 import './CommentForm.scss';
 
 export default function CommentForm(props) {
    const { publication } = props;
+
+   const [addComment] = useMutation(ADD_COMMENT);
 
    const formik = useFormik({
       initialValues: {
@@ -15,8 +19,20 @@ export default function CommentForm(props) {
       validationSchema: Yup.object({
          comment: Yup.string().required(),
       }),
-      onSubmit: (formData) => {
-         console.log(formData);
+      onSubmit: async (formData) => {
+         try {
+            await addComment({
+               variables: {
+                  input: {
+                     idPublication: publication.id,
+                     comment: formData.comment,
+                  },
+               },
+            });
+            formik.handleReset();
+         } catch (error) {
+            console.error(error);
+         }
       },
    });
    return (
